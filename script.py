b2cb7d437495c32184256845110a9db1be752df1
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-08-31 17:21:40 -0400

    Actualización Script
    se actualiza el script para rellenar la base de datos automáticamente

--- script.py
+++ script.py
@@ -1,98 +1,89 @@
 from faker import Faker
 from random import randint
 from random import choice
+import psycopg2
 
 fake = Faker()
 
-###PAIS
-paises = ""
-for k in range(21):
-    paises += "INSERT INTO pais (nombre, id_pais) values ('{}','{}') ;\n".format(fake.country(), k)
-print(paises)
+try:
+    connection = psycopg2.connect(
+        host="localhost", user="postgres", password="123", database="Aerolineas")
+    cursor = connection.cursor()
 
-###CLIENTE
-clientes = ""
-for k in range(20):
-    clientes += "INSERT INTO cliente (id_cliente, nombre, id_pais, dni) values ('{}','{}','{}','{}');\n".format(k, fake.name(), randint(0,20), fake.ean(length=8))
-print(clientes)
+    ### PAIS
+    for k in range(21):
+        cursor.execute(
+            "INSERT INTO pais (nombre, id_pais) VALUES(%s,%s)", (fake.country(), k))
 
-###CLASE
-listaclases = ["Economy", "Premium economy", "Business" , "First class"]
-clases = ""
-id = 0
-for k in listaclases:
-    clases += "INSERT INTO clase (id_clase, nombre) values ('{}','{}');\n".format(id, k)
-    id+=1
-print(clases)
+    ### CLIENTE
+    for k in range(20):
+        cursor.execute("INSERT INTO cliente (id_cliente, nombre, id_pais, dni) VALUES(%s,%s,%s,%s)",(k, fake.name(), randint(0, 20), fake.ean(length=8)))
 
-###MODELO (de AVION)
-listamodelos = ["Boeing 737", "Boeing 747", "Airbus 319", "Airbus 320", "Airbus 321"]
-modelos = ""
-id = 0
-for k in listamodelos:
-    modelos += "INSERT INTO modelo (id_modelo, nombre) values ('{}','{}');\n".format(id, k)
-    id+=1
-print(modelos)
+    ### CLASE
+    listaclases = ["Economy", "Premium economy", "Business", "First class"]
+    id = 0
+    for k in listaclases:
+        cursor.execute(
+            "INSERT INTO clase (id_clase, nombre) VALUES(%s,%s)", (id, k))
+        id += 1
 
-###COMPAÑIAS
-listacompanias = ["LATAM Airlines", "Sky Airline", "JetSmart", "United Airlines", "American Airlines", "Air France"]
-companias = ""
-id = 0
-for k in listacompanias:
-    companias += "INSERT INTO compania (id_compania, nombre) values ('{}','{}');\n".format(id, k)
-    id+=1
-print(companias)
+    ### MODELO (de AVION)
+    listamodelos = ["Boeing 737", "Boeing 747","Airbus 319", "Airbus 320", "Airbus 321"]
+    id = 0
+    for k in listamodelos:
+        cursor.execute(
+            "INSERT INTO modelo (id_modelo, nombre) VALUES(%s,%s)", (id, k))
+        id += 1
 
-###AVION
-aviones = ""
-id = 0
-for k in range(20):
-    aviones += "INSERT INTO avion (id_avion, id_modelo, id_compania, capacidad) values ('{}','{}','{}','{}');\n".format(k, randint(0, len(listamodelos)-1), randint(0, len(listacompanias)-1), randint(160, 480))
-    id+=1
-print(aviones)
+    ### COMPAÑIAS
+    listacompanias = ["LATAM Airlines", "Sky Airline", "JetSmart", "United Airlines", "American Airlines", "Air France"]
+    id = 0
+    for k in listacompanias:
+        cursor.execute(
+            "INSERT INTO compania (id_compania, nombre) VALUES(%s,%s)", (id, k))
+        id += 1
+    
+    ### AVION
+    id = 0
+    for k in range(20):
+        cursor.execute(
+            "INSERT INTO avion (id_avion, id_modelo, id_compania, capacidad) VALUES(%s,%s,%s,%s)", (id, randint(0, len(listamodelos)-1), randint(0, len(listacompanias)-1), randint(160, 480)))
+        id += 1
+        
+    # EMPLEADO
+    id = 0
+    for k in range(40):
+        cursor.execute(
+            "INSERT INTO empleado (id_empleado, dni, nombre, sueldo,id_compania) VALUES(%s,%s,%s,%s,%s)", (id, fake.ean(length=8), fake.name(), randint(1000, 8000), randint(0, len(listacompanias)-1)))
+        id += 1
+        
+    # VUELO
+    id = 0
+    for k in range(20):
+        cursor.execute(
+            "INSERT INTO vuelo (id_vuelo,embarque,id_origen, id_destino) values(%s,%s,%s,%s)", (id, fake.date_time_between(start_date="-1y", end_date="now", tzinfo=None), randint(0, len(listacompanias)-1), randint(0, len(listacompanias)-1)))
+        id += 1
 
-###EMPLEADO
-empleados = ""
-id = 0
-for k in range(40):
-    empleados += "INSERT INTO empleado (id_empleado, dni, nombre, sueldo) values ('{}','{}','{}','{}');\n".format(k, fake.ean(length=8), fake.name(), randint(1000, 8000))
-    id+=1
-print(empleados)
+    # PASAJE
+    id = 0
+    for k in range(20):
+        cursor.execute(
+            "INSERT INTO pasaje (id_pasaje, id_viaje, id_clase) values(%s,%s,%s)", (id, randint(0, 19), randint(0, len(listaclases)-1)))
+        id += 1
 
-###VUELO
-vuelos = ""
-id = 0
-for k in range(20):
-    vuelos += "INSERT INTO vuelo (id_vuelo, embarque, id_origen, id_destino) values ('{}','{}','{}','{}');\n".format(id, fake.date_time_between(start_date="-1y", end_date="now", tzinfo=None), randint(0, len(listamodelos)-1), randint(0, len(listacompanias)-1))
-    id+=1
-print(vuelos)
+    # COMPRA
+    id = 0
+    for k in range(20):
+        cursor.execute(
+            "INSERT INTO compra (id_compra, monto, fecha , id_cliente, id_pasaje) values(%s,%s,%s,%s,%s)", (id, randint(1000, 8000), fake.date(), randint(0, 19), randint(0, 19)))
+        id += 1
 
-###PASAJE
-pasajes = ""
-id = 0
-for k in range(20):
-    pasajes += "INSERT INTO pasaje (id_pasaje, id_viaje, id_clase) values ('{}','{}','{}');\n".format(
-        id, randint(0, 19), randint(0, len(listaclases)-1))
-    id += 1
-print(pasajes)
+    # log_vuelo_avion
 
+    # log_vuelo_empleado
 
-###COMPRA
+    connection.commit()
+    print('Base de datos rellenada')
 
-compra = ""
-id = 0
-for k in range(20):
-    compra += "INSERT INTO compra (id_compra, monto, fecha , id_cliente, id_pasaje) values ('{}','{}','{}','{}','{}');\n".format(id, randint(1000, 8000), fake.date(), randint(0, 19), randint(0, 19))
-    id+=1
-print(compra) 
-
-
-###log_vuelo_avion
-
-###log_vuelo_empleado
-
-
-#JUNTAR_TODO
-with open("scriptAerolinea.sql", "w", errors="ignore") as f:
-    f.write(paises)
-
+except Exception as e:
+    print(e)
