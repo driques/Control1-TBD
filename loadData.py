311835c72bb89871f64b733774d58a95226ce494
darsmen32@gmail.com <darsmen32@gmail.com>
2022-09-03 16:16:25 -0400

    Faker para DBv8 final

--- loadData.py
+++ loadData.py
@@ -7,7 +7,7 @@ fake = Faker()
 
 try:
     connection = psycopg2.connect(
-        host="localhost", user="postgres", password="admin", database="aerov8")
+        host="localhost", user="postgres", password="admin", database="aerov3")
     cursor = connection.cursor()
 
     ###### Delete ######
@@ -21,6 +21,10 @@ try:
     cursor.execute("TRUNCATE TABLE modelo CASCADE")
     cursor.execute("TRUNCATE TABLE pais CASCADE")   
     cursor.execute("TRUNCATE TABLE vuelo CASCADE")
+    cursor.execute("TRUNCATE TABLE cargo CASCADE")
+    cursor.execute("TRUNCATE TABLE log_empleado_cargo CASCADE")
+    cursor.execute("TRUNCATE TABLE log_vuelo_avion CASCADE")
+    cursor.execute("TRUNCATE TABLE log_vuelo_empleado CASCADE")
     connection.commit()
 
     print('Datos de tablas existentes eleminadas')
@@ -80,7 +84,7 @@ try:
         cursor.execute(
             "INSERT INTO vuelo (id_vuelo,embarque,id_origen, id_destino) values(%s,%s,%s,%s)", (id, fake.date_time_between(start_date="-5y", end_date="now", tzinfo=None), randint(0, len(listacompanias)-1), randint(0, len(listacompanias)-1)))
         id += 1
-    print("aqui")
+
 
     
     # PASAJE
@@ -89,18 +93,43 @@ try:
         cursor.execute(
             "INSERT INTO pasaje (id_pasaje,id_vuelo, id_clase,precio) values(%s,%s,%s,%s)", (id, randint(0, 19), randint(0, len(listaclases)-1),randint(30,2000)))
         id += 1
-    print("aca")
 
+    # cargos
+    cargos = ["piloto","mecanico","asafata"]
+    id = 0
+    for k in range(3):
+        cursor.execute(
+            "INSERT INTO cargo (id_cargo, nombre) values(%s,%s)", (id, cargos[id]))
+        id += 1
     # COMPRA
     id = 0
     for k in range(20):
         cursor.execute(
             "INSERT INTO compra (id_compra, monto, fecha , id_cliente, id_pasaje) values(%s,%s,%s,%s,%s)", (id, randint(1000, 8000), fake.date_time_between(start_date="-5y", end_date="now", tzinfo=None), randint(0, 19), randint(0, 19)))
         id += 1
+
     # log_vuelo_avion
+    id = 0
+    for k in range(20):
+        cursor.execute(
+            "INSERT INTO log_vuelo_avion (id_vuelo, id_avion, id_avion_vuelo) values(%s,%s,%s)", (randint(0, 19), randint(0, 19), id))
+        id += 1
+    #log_vuelo_empleado
+    id = 0
+    for k in range(20):
+        cursor.execute(
+            "INSERT INTO log_vuelo_empleado (id_vuelo, id_empleado, id_log_vuelo_empleado) values(%s,%s,%s)", (randint(0, 19), randint(0, 40), id))
+        id += 1
 
-    # log_vuelo_empleado
 
+    # log_empleado_cargo
+    id = 0
+    for k in range(20):
+        cursor.execute(
+            "INSERT INTO log_empleado_cargo (id_log_emp_car, id_empleado, id_cargo) values(%s,%s,%s)", (id, randint(0, 40),randint(0,2)))
+        id += 1
+
+    
     connection.commit()
     print('Base de datos rellenada con nuevos datos')
 
